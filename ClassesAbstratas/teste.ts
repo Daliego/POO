36533d19848fc6abb914a75394911a4f49245a4c
Diego de Sousa Araujo <diesgo.araujo@gmail.com>
2023-08-04 11:40:05 -0300

    Classes abstratas content

---
+++ ClassesAbstratas/teste.ts
@@ -0,0 +1,9 @@
+class Teste {
+    private _itens: IInventoryItens;
+    constructor(itens: IInventoryItens) {
+        this._itens = itens;
+    }
+
+}
+
+let itens: Teste = new Teste(new InventoryIntens());
